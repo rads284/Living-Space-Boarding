455789c07c0ebbd8141492e4ee9e76d2413bb1a5
rads284 <agarwal.radhika098@gmail.com>
2019-11-23 23:38:19 +0530

    DB Fixed

--- database/create_db.py
+++ database/create_db.py
@@ -1,10 +1,11 @@
 import csv, sqlite3
 
-con = sqlite3.connect(":memory:")
+con = sqlite3.connect("livingspace.db")
 cur = con.cursor()
-cur.execute("CREATE TABLE Warden (FullName,Id,PhoneNumber,Password,Email, PRIMARY KEY(Id));") # use your column names here
-with open('User_Parent.csv','r',encoding="utf8") as fin: # `with` statement available in 2.5+
-    # csv.DictReader uses first line in file for column headings by default
+cur.execute('PRAGMA foreign_keys = ON')
+
+cur.execute("CREATE TABLE IF NOT EXISTS Warden (FullName,Id,PhoneNumber,Password,Email, PRIMARY KEY(Id));") # use your column names here
+with open('Warden.txt','r',encoding="utf8") as fin: 
     dr = csv.DictReader(fin) # comma is default delimiter
     to_db = [(i['FullName'], i['Id'],i['PhoneNumber'],i['Password'],i['Email']) for i in dr]
 cur.executemany("INSERT INTO Warden (FullName,Id,PhoneNumber,Password,Email) VALUES (?, ?, ?, ?, ?);", to_db)
@@ -12,26 +13,28 @@ con.commit()
 # cur = con.cursor()
 # cur.execute("SELECT * FROM user;")
 # print(cur.fetchone  ())
-
-cur.execute("CREATE TABLE Student(w_id,Id,FullName, FOREIGN KEY(w_id) REFERENCES Warden(Id) ON DELETE CASCADE ON UPDATE NO ACTION);") # use your column names here
-with open('Student.txt','r',encoding="utf8") as fin: # `with` statement available in 2.5+
-    # csv.DictReader uses first line in file for column headings by default
-    dr = csv.DictReader(fin) # comma is default delimiter
-    to_db = [(i['w_id'], i['Id'],i['FullName']) for i in dr]
-cur.executemany("INSERT INTO Student (w_id,Id,FullName) VALUES (?, ?, ?);", to_db)
-con.commit()
-
-cur.execute("CREATE TABLE Parent (FullName,Id,s_id,Password,Email,PhoneNumber, PRIMARY KEY(Id), FOREIGN KEY(s_id) REFERENCES Student(Id) ON DELETE CASCADE ON UPDATE NO ACTION );") # use your column names here
+cur.execute("CREATE TABLE IF NOT EXISTS Parent (FullName,Id,Password,Email,PhoneNumber, PRIMARY KEY(Id) );") # use your column names here
 with open('Parent.txt','r',encoding="utf8") as fin: # `with` statement available in 2.5+
     # csv.DictReader uses first line in file for column headings by default
     dr = csv.DictReader(fin) # comma is default delimiter
-    to_db = [(i['FullName'], i['Id'],i['s_id'],i['Password'],i['Email'],i['PhoneNumber']) for i in dr]
-cur.executemany("INSERT INTO Parent (FullName,Id,s_id,Password,Email,PhoneNumber) VALUES (?, ?, ?, ?, ?, ?);", to_db)
+    to_db = [(i['FullName'], i['Id'],i['Password'],i['Email'],i['PhoneNumber']) for i in dr]
+cur.executemany("INSERT INTO Parent (FullName,Id,Password,Email,PhoneNumber) VALUES (?, ?, ?, ?, ?);", to_db)
 con.commit()
 
-cur.execute("CREATE TABLE Leave (id,s_id,w_id,p_id,CheckIn,CheckOut,ApprovedP,ApprovedW, PRIMARY KEY(id), FOREIGN KEY(s_id) REFERENCES Student(Id) ON DELETE CASCADE ON UPDATE NO ACTION, FOREIGN KEY(w_id) REFERENCES Warden(Id) ON DELETE CASCADE ON UPDATE NO ACTION, FOREIGN KEY(p_id) REFERENCES Parent(Id) ON DELETE CASCADE ON UPDATE NO ACTION );") # use your column names here
+cur.execute("CREATE TABLE IF NOT EXISTS Student(w_id,Id,FullName,p_id,PRIMARY KEY(Id), FOREIGN KEY(w_id) REFERENCES Warden(Id) ON DELETE CASCADE ON UPDATE NO ACTION,FOREIGN KEY(p_id) REFERENCES Parent(Id) ON DELETE CASCADE ON UPDATE NO ACTION);") # use your column names here
+with open('Student.txt','r',encoding="utf8") as fin: # `with` statement available in 2.5+
+    # csv.DictReader uses first line in file for column headings by default
+    dr = csv.DictReader(fin) # comma is default delimiter
+    to_db = [(i['w_id'], i['Id'],i['FullName'],i['p_id']) for i in dr]
+cur.executemany("INSERT INTO Student (w_id,Id,FullName,p_id) VALUES (?, ?, ?, ?);", to_db)
 con.commit()
+
+cur.execute("CREATE TABLE IF NOT EXISTS Leave (id AUTO INCREMENT,s_id,CheckIn,CheckOut,ApprovedP,ApprovedW, PRIMARY KEY(id), FOREIGN KEY(s_id) REFERENCES Student(Id) ON DELETE CASCADE ON UPDATE NO ACTION);")
+
+row= ('99', '19-11-2019', '21-11-2019', 'Yes', 'No')
+
+cur.execute("INSERT INTO Leave (s_id,CheckIn,CheckOut,ApprovedP,ApprovedW) VALUES(?,?,?,?,?);",row)
 # Insert only when student is absent.
-cur.execute("CREATE TABLE Attendance (CDate, s_id, PRIMARY KEY(CDate,s_id), FOREIGN KEY(s_id) REFERENCES Student(Id) ON DELETE CASCADE ON UPDATE NO ACTION)")
+cur.execute("CREATE TABLE IF NOT EXISTS Attendance (CDate, s_id, PRIMARY KEY(CDate,s_id), FOREIGN KEY(s_id) REFERENCES Student(Id) ON DELETE CASCADE ON UPDATE NO ACTION)")
 con.commit()
 con.close()
