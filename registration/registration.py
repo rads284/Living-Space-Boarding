85b251f484260cf32258e80712f8f3cc964227fb
prajwalkirankumar <prajwalkirankumar@yahoo.com>
2019-11-24 16:27:55 +0530

    Some changes

--- registration/registration.py
+++ registration/registration.py
@@ -2,17 +2,24 @@ from pymongo import MongoClient
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-MONGO_CLIENT = MongoClient('localhost', 27017)
-DATABASE = MONGO_CLIENT.db
-USER_DETAILS = DATABASE.details
+conn = sqlite3.connect('livingspace.db', check_same_thread=False)
+c = conn.cursor()
 
-@app.route('/',methods=['POST'])
+@app.route('/register',methods=['POST'])
 def hello():
 	try:
-		USER_DETAILS.insert_one(request.json)
-		return "Added to Database Successfully!",200
+		if(request["user-type"]=="warden"):
+			c.execute("INSERT INTO Warden (FullName,Id,PhoneNumber,Password,Email) VALUES (?, ?, ?, ?, ?);",request.json["full-name"],request.json["id"],request.json["phone-number"],request.json["password"],request.json["email"])
+		elif(request["user-type"]=="parent"):
+			c.execute("INSERT INTO Parent (FullName,Id,PhoneNumber,Password,Email,) VALUES (?, ?, ?, ?, ?);",request.json["full-name"],request.json["id"],request.json["phone-number"],request.json["password"],request.json["email"])
+		elif(request["user-type"]=="student"):
+			c.execute("INSERT INTO Student (w_id,Id,FullName,p_id) VALUES (?, ?, ?, ?);",request.json["w_id"],request.json["id"],request.json["full-name"],request.json["p_id"])
+
+		conn.commit()
+		conn.close()
+		return jsonify("Added to Database Successfully!"),200
 	except:
-		return "Database Error!",400
+		return jsonify("Database Error!"),400
 
 if __name__ == '__main__':
-    app.run(host="localhost",port=8080,debug=True)
+    app.run(host="0.0.0.0",port=8080,debug=True)
