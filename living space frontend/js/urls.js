7071a0189331cc8efd3502e401d64791bb0c7138
prajwalkirankumar <prajwalkirankumar@yahoo.com>
2019-11-25 13:56:11 +0530

    URLS.js

---
+++ living space frontend/js/urls.js
@@ -0,0 +1,9 @@
+var URL = {
+	registerURL : "http://localhost:8000/register",
+	signinURL : "http://localhost:8001/login",
+	leaverequestparentURL : "http://localhost:8002/notify_parent",
+	leaverequestwardenURL : "http://localhost:8002/notify_warden",
+	leaveapprovalURL : "http://localhost:8002/notify_warden",
+	viewattendanceURL : "http://localhost:8003/viewattendance",
+	markattendanceURL : "http://localhost:8003/markattendance"
+}
